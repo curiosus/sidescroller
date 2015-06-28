0dad5e443b362c1418dde8eb0269804458db7909
curiosus <jepeterson@gmail.com>
2015-06-27 18:22:57 -0600

    got scrolling working in both directions

--- main.cpp
+++ main.cpp
@@ -2,64 +2,59 @@
 #include <SFML/Window.hpp>
 #include <SFML/Graphics.hpp>
 
-using sf::RenderWindow;
-using sf::FloatRect;
-using sf::Color;
-using sf::Event;
-using sf::Keyboard;
-using sf::Texture;
-using sf::Sprite;
-using sf::Vector2u;
-using sf::Vector2i;
-using sf::Vector2f;
-using std::cout;
-using std::endl;
-
-
-const Vector2u windowSize{1200, 900};
+const sf::Vector2u windowSize{1200, 900};
 
 int main() {
 
-  Texture backgroundTexture;
+  sf::Texture backgroundTexture;
   if (!backgroundTexture.loadFromFile("background.png")) {
-    cout << "whoops" << endl;
+    std::cout << "fuck!!!" << std::endl;
     return 1;
   }
-  Sprite backgroundSprite;
+
+  const sf::Vector2f& startPosition{0.0f, 0.0f};
+  const sf::Vector2f& endPosition{-1790.0f, 0.0f};
+  sf::Sprite backgroundSprite;
   backgroundSprite.setTexture(backgroundTexture);
 
-  Texture playerTexture;
+  const sf::Vector2f& moveRight{-10.0f, 0.f};
+  const sf::Vector2f& moveLeft{10.0f, 0.0f};
+
+  sf::Texture playerTexture;
   if (!playerTexture.loadFromFile("player.png")) {
-    cout << "whoops2" << endl;
+    std::cout << "fuck fuck fuck!!!" << std::endl;
     return 1;
   }
-  Sprite playerSprite;
+  sf::Sprite playerSprite;
   playerSprite.setTexture(playerTexture);
-
-  playerSprite.setPosition(150.0,750.0);
-  const Vector2f& playerMovementRight{-10.f, 0.f};
-  const Vector2f& playerMovementLeft{10.f, 0.f};
-  //const Vector2f& playerMovementUp{0.f, -10.f};
-  //const Vector2f& playerMovementDown{0.f, 10.f};
+  playerSprite.setPosition(600.0f, 800.0f);
   
+  sf::RenderWindow window{{windowSize.x, windowSize.y}, "Side Scroller"};
   
-  
-  RenderWindow window{{windowSize.x, windowSize.y}, "Side Scroller"};
   while (window.isOpen()) {
-    window.clear(Color::Black);
-    Event event;
+    window.clear(sf::Color::Black);
+
+    //event polling
+    sf::Event event;
     while (window.pollEvent(event)) {
-      if (event.type == Event::Closed) {
+      if (event.type == sf::Event::Closed) {
         window.close();
         break;
-      } else if (event.type == Event::KeyPressed) {
-        if (event.key.code == Keyboard::Key::D) {
-           backgroundSprite.move(playerMovementRight);
+      }
+      if (event.type == sf::Event::KeyPressed) {
+        const sf::Vector2f& pos = backgroundSprite.getPosition();
+        std::cout << pos.x << std::endl; 
+        if (event.key.code == sf::Keyboard::Key::D) {
+          if (pos.x <= -1790.f) {
+            backgroundSprite.setPosition(startPosition); 
+          }
+          backgroundSprite.move(moveRight);
+        } else if (event.key.code == sf::Keyboard::Key::A) {
+          if (pos.x >= 0.f) {
+            backgroundSprite.setPosition(endPosition);
+          }   
+          backgroundSprite.move(moveLeft);
         }
-        if (event.key.code == Keyboard::Key::A) {
-           backgroundSprite.move(playerMovementLeft);
-        }
-        
       }
     }
 
@@ -67,9 +62,9 @@ int main() {
     window.draw(playerSprite);
     window.display();
     
-
   }
 
-
+  return 0;
 }
+
 
